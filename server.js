340a15574509907b6384430518e3ed451e5941a3
Julian Napolitan <julian.napolitan@gmail.com>
2019-01-10 09:57:58 -0800

    Console log sentiment score and user location

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
 const express = require('express');
 const http = require('http');
-const socketio = require('socket.io');
+// const socketio = require('socket.io');
 const path = require('path');
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 const app = express();
-const Twit = require('twit');
 const keys = require('./config/keys');
 
+const Twit = require('twit');
+const Sentiment = require('sentiment');
+
 const server = http.createServer(app);
-const io = socketio(server);
+// const io = socketio(server);
 
 app.use(bodyParser.json());
 
 server.listen(port, () => {
   console.log('server is listening');
 });
+
+let currentTweets = {}
 
 let twitter = new Twit({
   consumer_key: keys.consumer_key,
@@ -24,8 +28,20 @@ let twitter = new Twit({
   access_token_secret: keys.access_token_secret
 });
 
-const stream = twitter.stream('statuses/sample')
+const stream = twitter.stream('statuses/filter', {
+  track: 'government shutdown'
+});
+
+const sentiment = new Sentiment();
 
 stream.on('tweet', function (tweet) {
-  console.log(tweet);
+  // currentTweets[tweet.id] = {
+  //   text: tweet.text,
+  //   name: tweet.user.name,
+  //   location: tweet.user.location
+  // };
+  tweetSentimentScore = sentiment.analyze(tweet.text).score;
+  if (tweetSentimentScore !== 0 && tweet.user.location) {
+    console.log(tweetSentimentScore, tweet.user.location);
+  }
 });
