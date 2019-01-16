ac5de7c790fb4ee7629cb72b00cf72352465c40b
Julian Napolitan <julian.napolitan@gmail.com>
2019-01-15 19:04:41 -0800

    Update markers with D3 syntax

--- src/components/world_map.jsx
+++ src/components/world_map.jsx
@@ -18,7 +18,7 @@ export default class WorldMap extends Component {
     this.currentSearchTerm = '';
 
     // this.handleCountryClick = this.handleCountryClick.bind(this);
-    this.handleMarkerClick = this.handleMarkerClick.bind(this);
+    // this.handleMarkerClick = this.handleMarkerClick.bind(this);
 
     this.socket = socketIOClient("http://localhost:3000/");
     this.svgWidth = 1200;
@@ -53,6 +53,7 @@ export default class WorldMap extends Component {
   renderMap() {
     const svg = select(this.map)
       .append('svg')
+      .attr('id', 'svg-map')
       .attr("width", this.svgWidth)
       .attr("height", this.svgHeight)
       .attr("viewBox", `0 0 ${this.svgWidth} ${this.svgHeight}`);
@@ -70,19 +71,37 @@ export default class WorldMap extends Component {
     });
   }
 
+  updateMarkers() {
+    const svg = select(document.getElementById('svg-map'))
+    if (!svg) return;
+    const g = svg.append("g");
+
+    this.state.tweets.map((tweet, i) => {
+      g.append("circle")
+        .attr("cx", this.projection()(tweet.coordinates)[0])
+        .attr("cy", this.projection()(tweet.coordinates)[1])
+        .attr("r", Math.abs(tweet.sentiment) || 3)
+        .attr("fill", this.sentimentColor(tweet.sentiment));
+    });
+  }
+
   // handleCountryClick(countryIndex) {
   //   console.log("Clicked on country: ", this.state.worlddata[countryIndex]);
   // }
-  handleMarkerClick(markerIndex) {
-    const place = this.state.tweets[markerIndex].name;
-    const text = this.state.tweets[markerIndex].text;
-    alert(`${place}: ${text}`);
-  }
+  // handleMarkerClick(markerIndex) {
+  //   const place = this.state.tweets[markerIndex].name;
+  //   const text = this.state.tweets[markerIndex].text;
+  //   alert(`${place}: ${text}`);
+  // }
 
   componentDidMount() {
     this.map = document.getElementById('map');
     this.createMap();
-    // this.openSocket();
+    this.openSocket();
+  }
+
+  componentDidUpdate() {
+    this.updateMarkers();
   }
 
   componentWillUnmount() {
@@ -136,50 +155,40 @@ export default class WorldMap extends Component {
     });
   }
 
-  radarPing(d) {
-    const p = this.projection([d.coordinates[0], d.coordinates[1]]);
-    const x = p[0];
-    const y = p[1];
-    for (var i = 1; i < 5; i += 1) {
-        select(this.markers)
-          .append("circle")
-          .classed("radar-ping", true)
-          .attr("cx", x)
-          .attr("cy", y)
-          .attr("r", this.startPingRadius - (this.pingThickness / 2))
-          .style("stroke-width", this.pingThickness / i)
-          .style('stroke', this.sentimentColor(d.sentiment))
-        .transition()
-          .delay(Math.pow(i, 2.5) * 50)
-          .duration(1000).ease('quad-in')
-          .attr("r", this.endPingRadius)
-          .style("stroke-opacity", 0)
-          .style('stroke', this.sentimentColor(d.sentiment));
-    }
-  }
+  // radarPing(d) {
+  //   const p = this.projection([d.coordinates[0], d.coordinates[1]]);
+  //   const x = p[0];
+  //   const y = p[1];
+  //   for (var i = 1; i < 5; i += 1) {
+  //       select(this.map)
+  //         .append("circle")
+  //         .classed("radar-ping", true)
+  //         .attr("cx", x)
+  //         .attr("cy", y)
+  //         .attr("r", this.startPingRadius - (this.pingThickness / 2))
+  //         .style("stroke-width", this.pingThickness / i)
+  //         .style('stroke', this.sentimentColor(d.sentiment))
+  //       .transition()
+  //         .delay(Math.pow(i, 2.5) * 50)
+  //         .duration(1000).ease('quad-in')
+  //         .attr("r", this.endPingRadius)
+  //         .style("stroke-opacity", 0)
+  //         .style('stroke', this.sentimentColor(d.sentiment));
+  //   }
+  // }
 
-  updateMarkers() {
-      select(this.markers)
-        .selectAll("circle")
-        .data(this.state.tweets)
-        .enter()
-        .append("circle")
-        .classed("point", true)
-        .attr("r", 3)
-        .each((d) => {
-          this.radarPing(d);
-        });
-      // <circle
-      //   key={`marker-${i}`}
-      //   cx={this.projection()(tweet.coordinates)[0]}
-      //   cy={this.projection()(tweet.coordinates)[1]}
-      //   r={Math.abs(tweet.sentiment) || 3}
-      //   fill={this.sentimentColor(tweet.sentiment)}
-      //   className="marker"
-      //   onClick={() => this.handleMarkerClick(i)}
-      // />
-    // ))
-  }
+  // updateMarkers() {
+  //     select(this.map)
+  //       .selectAll("circle")
+  //       .data(this.state.tweets)
+  //       .enter()
+  //       .append("circle")
+  //       .classed("point", true)
+  //       .attr("r", 3)
+  //       .each((d) => {
+  //         this.radarPing(d);
+  //       });
+  // }
 
   render() {
     return <>
