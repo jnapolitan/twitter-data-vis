b0dbf498fe5f77666ddf5aa225d90976a2718ccf
Julian Napolitan <julian.napolitan@gmail.com>
2019-01-15 22:07:42 -0800

    Heroku config

--- src/server/server.js
+++ src/server/server.js
@@ -4,11 +4,19 @@ const socketio = require('socket.io');
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 3001;
 const app = express();
+const path = require('path');
 
 const server = http.createServer(app);
 const io = socketio(server);
 
 app.use(bodyParser.json());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
 
 require('./routes/tweets.js')(app, io);
 
