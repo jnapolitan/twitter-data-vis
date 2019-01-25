eeaed359f4a615dbfa41347a130d213166ea534b
Julian Napolitan <julian.napolitan@gmail.com>
2019-01-25 15:33:13 -0800

    Implement more maintained Twit npm module

--- src/server/routes/tweets.js
+++ src/server/routes/tweets.js
@@ -1,15 +1,15 @@
 const keys = require('../../config/keys');
-const Twitter = require('twitter');
+const Twit = require('twit');
 const Sentiment = require('sentiment');
 
 
 module.exports = (app, io) => {
 
   // Configure Twitter streaming client
-  const twitter = new Twitter({
+  const twitter = new Twit({
     consumer_key: keys.consumer_key,
     consumer_secret: keys.consumer_secret,
-    access_token_key: keys.access_token_key,
+    access_token: keys.access_token_key,
     access_token_secret: keys.access_token_secret
   });
 
@@ -21,52 +21,48 @@ module.exports = (app, io) => {
 
   // Begin Twitter stream if search term is present
   const stream = () => {
-    if (app.locals.searchTerm !== '') {
-      twitter.stream('statuses/filter', {
+    if (searchTerm !== '') {
+      twitterStream = twitter.stream('statuses/filter', {
         track: searchTerm
-      }, (stream) => {
-        stream.on('data', (tweet) => {
-          // Filter tweets that aren't geo-tagged
-          if (tweet.place) {
-            // Append sentiment data to tweet object
-            tweet.sentiment = sentiment.analyze(tweet.text);
-            // Send data to frontend with socket.io
-            emitData({
-              name: tweet.place.full_name,
-              coordinates: tweet.place.bounding_box.coordinates[0][0],
-              text: tweet.text,
-              sentiment: tweet.sentiment.score
-            });
-          }
-        });
-
-        stream.on('error', (error) => {
-          console.log(error);
-        });
-
-        // Assign stream to variable for easier management
-        twitterStream = stream;
       });
-    }
-  };
+
+      twitterStream.on('message', (tweet) => {
+        console.log(tweet);
+        // Filter tweets that aren't geo-tagged
+        if (tweet.place) {
+          // Append sentiment data to tweet object
+          tweet.sentiment = sentiment.analyze(tweet.text);
+          // Send data to frontend with socket.io
+          emitData({
+            name: tweet.place.full_name,
+            coordinates: tweet.place.bounding_box.coordinates[0][0],
+            text: tweet.text,
+            sentiment: tweet.sentiment.score
+          });
+        }
+      });
+
+      twitterStream.on('error', (error) => {
+        console.log(error);
+      });
+      }
+    };
 
   // Route for setting search term and updating stream
   app.post('/setSearchTerm', (req, res) => {
     const term = req.body.term;
     searchTerm = term;
-    if (twitterStream) twitterStream.destroy();
+    if (twitterStream) twitterStream.stop();
     stream();
     console.log('Stream updated for', searchTerm);
   });
 
   // Route for manually destroying the stream
   app.post('/destroy', (req, res) => {
-    
-    if (twitterStream)  {
-      console.log('we have a stream');
-      twitterStream.destroy();
+    if (twitterStream) {
+      twitterStream.stop();
+      console.log('Stream ended');
     }
-    console.log('Stream ended');
   });
 
   //Establishes socket connection.
