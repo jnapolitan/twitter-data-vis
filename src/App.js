0b579815dc846db2b8cdf3b26f340f7b4a23b1d6
Julian Napolitan <julian.napolitan@gmail.com>
2019-02-04 19:43:55 -0800

    Extract site description into its own component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import WorldMap from './components/world_map';
+import Description from './components/description';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Description />
         <WorldMap />
       </div>
     );
--- src/components/world_map.jsx
+++ src/components/world_map.jsx
@@ -180,22 +180,6 @@ export default class WorldMap extends Component {
             placeholder='Search...' />
         </form>
         <div id='map' />
-        <div className='info-panel'>
-          <h3>What is this?</h3>
-          <p>This project uses Twitter's Stream API, D3 geo visualization, and sentiment analysis to show how people are talking about particular subjects around the world.</p>
-          <p>Enter a subject, and as people tweet about that subject you will see dots appear on the map.</p>
-          <p><span className='red'><strong>Red</strong></span> dots indicate negative sentiment.</p>
-          <p><span className='green'><strong>Green</strong></span> dots indicate positive sentiment.</p>
-          <p><span className='gray'><strong>Gray</strong></span> dots indicate neutral sentiment.</p>
-          <p>The <strong>size</strong> of the dots indicate the severity of sentiment. The smaller the dot, the less severe the sentiment.</p>
-          <h4>*Please note*</h4>
-          <p>This is an ongoing project with more visualizations to come. If it seems like there aren't many (or any) Tweets being visualized, it's likely due to one of the following:</p>
-          <ul>
-            <li>Not as many Tweets are geo-tagged as you might think (and we're working on non-geotagged visualizations to fill the void).</li>
-            <li>Twitter's free API (although a fantastic resource) can be somewhat limited.</li>
-          </ul>
-          <p>If you really want to see this project in action, type in "Trump" and let it run for a minute or two.</p>
-        </div>
       </>;
   }
 
